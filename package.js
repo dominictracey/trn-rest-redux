b8d16313a97a7a418356d75dea85b44e7fcceda5
xavcz <xavier.cazalot@gmail.com>
2017-02-16 12:31:49 +0100

    bump version

--- package.js
+++ package.js
@@ -8,7 +8,7 @@ Package.onUse( function(api) {
   api.versionsFrom("METEOR@1.0");
 
   api.use([
-    'nova:core@1.0.0',
+    'nova:core@1.1.0',
   ]);
 
   api.mainModule("lib/modules.js", ["client", "server"]);
