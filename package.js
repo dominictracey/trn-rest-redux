de3123e54b90f96e7b4dc3e3c94429a72a627486
Dominic Tracey <dominic.tracey@rugby.net>
2017-03-23 13:45:11 -0400

    dev update and version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "trn:rest-redux",
-  version: "1.0.0",
+  version: "1.3.0",
 });
 
 Package.onUse( function(api) {
@@ -8,7 +8,7 @@ Package.onUse( function(api) {
   api.versionsFrom("METEOR@1.0");
 
   api.use([
-    'nova:core@1.1.0',
+    'vulcan:core@1.3.0',
   ]);
 
   api.mainModule("lib/modules.js", ["client", "server"]);
